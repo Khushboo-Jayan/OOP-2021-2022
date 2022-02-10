64c7e77db26c0bb20111962e45d646395cdc4bd6
Khushboo Jayan <khushboojayan@gmail.com>
2022-02-10 15:54:38 +0000

    creating player try1

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -11,55 +11,65 @@ public class BugZap extends PApplet {
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
 		
 	}
+	
+	float playerX = 225;
+	float playerY = 450;
+	float playerSpeed = 5;
+	float playerWidth = 40;
+	float halfPlayerWidth = playerWidth / 2;
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+	float bugX, bugY, bugWidth = 30;
+	float halfBugWidth = bugWidth / 2;
+
+	int score = 0;
+
+	void drawPlayer(float x, float y, float w){
+		stroke(255);
+		float playerHeight = w / 2;
+		line(x - halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight);
+		line(x - halfPlayerWidth, y + playerHeight, x - halfPlayerWidth, y + playerHeight * 0.5f);
+		line(x + halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight * 0.5f);
+
+		line(x - halfPlayerWidth, y + playerHeight * 0.5f, x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+		line(x + halfPlayerWidth, y + playerHeight * 0.5f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+
+		line(x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f, x + (halfPlayerWidth * 0.8f),
+				y + playerHeight * 0.3f);
+
+		line(x, y, x, y + playerHeight * 0.3f);
+	}
+
+
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+			if (playerX > halfPlayerWidth) {
+				playerX -= playerSpeed;
+			}
+		}
+		if (keyCode == RIGHT) {
+			if (playerX < width - halfPlayerWidth) {
+				playerX += playerSpeed;
+			}
+		}
+		if (keyCode == ' ')
+		{
+			if (playerX > bugX - halfBugWidth && playerX < bugX + halfBugWidth)
+			{
+				line(playerX, playerY, playerX, bugY);
+				score ++;
+			}
+			else
+			{
+				line(playerX, playerY, playerX, 0);
+			}
+		}
+	}
+
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
-		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
-
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		drawPlayer(playerX, playerY, playerWidth);
 	}
 }
