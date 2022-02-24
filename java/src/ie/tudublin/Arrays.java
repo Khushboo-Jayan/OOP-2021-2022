ba3ddac1f5503fc1febecb2a600a77062a64766c
Khushboo Jayan <khushboojayan@gmail.com>
2022-02-24 09:58:26 +0000

    barchart

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -6,6 +6,8 @@ public class Arrays extends PApplet {
     float[] rainfall = { 45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58 };
     String[] months = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
 
+    int mode = 2;
+
     public void settings() {
         size(500, 500);
     }
@@ -48,21 +50,81 @@ public class Arrays extends PApplet {
         background(0);
         colorMode(HSB);
 		// //int bars = 10;
-		float bars = rainfall.length ; // changes as he cursor is hovered. 
-		float w = width / (float) bars;
+		// float bars = rainfall.length ; // changes as he cursor is hovered. 
+		// float w = width / (float) bars;
 		// //float colorGap = 255/(float) bars;
 		// for(int i =0; i<bars; i++){
         //     noStroke();            
         //     rect(map(i, 0, bars, 0, 500), 0, w, rainfall[i]*3);
         //     fill(map(i, 0, bars, 0, 255), 255,255);
         // }
-        for(int i =0; i<rainfall.length; i++){
-            float x = map(i, 0, rainfall.length, 0, width);
-            // int c = (int) map(i, 0, rainfall.length, 0, 255);
-            rect(x, height, w, -rainfall[i]);
-            fill(map(i, 0, bars, 0, 255), 255,255);
-            textAlign(CENTER);;
-            text(months[i], x+(w/2), height-50);
-            }
+        // for(int i =0; i<rainfall.length; i++){
+        //     float x = map(i, 0, rainfall.length, 0, width);
+        //     // int c = (int) map(i, 0, rainfall.length, 0, 255);
+        //     rect(x, height, w, -rainfall[i]);
+        //     fill(map(i, 0, bars, 0, 255), 255,255);
+        //     textAlign(CENTER);;
+        //     text(months[i], x+(w/2), height-50);
+        //     }
+
+        switch(mode){
+            case 1: 
+                background(0);
+                float border = width*0.2f;
+                //Draw axis
+                stroke(255);
+                line(border, border, border, height-border);
+                line(border, height-border, width- border, height);
+                for(int i=0; i <= 120; i++){
+                    float y = map(i, 0, 120, height-border, border);
+                    line(border-5, y, border, y);
+                    fill(255);
+                    textAlign(CENTER,CENTER);
+                    text(i, border/2, y);
+                }
+
+                float w = width/(float) rainfall.length;
+
+                for(int i=0; i < rainfall.length; i++){
+                    float x = map(i, 0, rainfall.length, border, width);
+                    float c = map(i, 0, rainfall.length, 0, 255);
+                    stroke(0);
+                    fill(c, 255, 255);
+                    float h = map(rainfall[i], 0, 120, 0, -(height+(border*2.0f)));
+                    rect(x,height-border,w,h);
+                    text(months[i], x, (w/2), height-(border/2));
+                }
+                break;
+            case 2:
+                background(0);
+                float r = mouseX;
+                float cx = width/2;
+                float cy = width/2;
+                stroke(255);
+                noFill();
+                circle(cx, cy, r*2);
+                float tot = 0f;
+                for(float f:rainfall){
+                    tot+= f;
+                }
+                
+                float start =0f;
+
+                for(int i=0; i<rainfall.length;i++){
+                    float val = map(rainfall[i], 0, tot, 0, TWO_PI);
+                    float c = map(i, 0, rainfall.length, 0, 255);
+                    noStroke();
+                    fill(c, 255, 255);                    
+                    arc(cx,cy,r*2,r*2,start,start+val, PIE);
+                    float theta = start + (val*0.5f);
+                    float x = cx + cos(theta) * (r*1.2f);
+                    float y = cy +sin(theta) * (r*1.2f);
+                    fill(255);
+                    text(months[i], x, y);
+                    start = start + val;
+                }               
+                break;
+        }       
+
     }
 }
