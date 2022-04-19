fde6da5b268cd3851e26c192807676e129e7c6cf
Khushboo Jayan <khushboojayan@gmail.com>
2022-04-19 23:25:11 +0100

    week 14

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,34 +1,13 @@
 package ie.tudublin;
 
-
-
 public class Main
 {
-
-    public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-    public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
 
     public static void arrays()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Arrays());
     }
-
-    public static void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-    }
-
 
     public void cat()
     {
@@ -56,9 +35,6 @@ public class Main
     public static void main(String[] args)
     {
         arrays();
-
-        // Tara Misty
-        // Tara Tara
         
     }
 }
